3787e936c07b1ef309651946e21279ee2837b43c
anil-kumar-automation <anil.singala@zensar.com>
2022-12-12 14:26:06 +0530

    Extent report with screenshot

--- src/main/java/listeners/ExtentTestNGIReporterListener.java
+++ src/main/java/listeners/ExtentTestNGIReporterListener.java
@@ -1,93 +1,34 @@
-/*
 package listeners;
-
+import BrowserFactory.DriverFactory;
 import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
-import org.testng.*;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import java.io.File;
+import java.io.IOException;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-// aventstack extent code it's supported with 5.0.8 version dependency ,notePoint if use this code for reports just comment 3.1.5 version in pom.xml
-
-public class ExtentTestNGIReporterListener implements IReporter {
-    private static final String OUTPUT_FOLDER = "Reports";
-    private static final String FILE_NAME = "Extent.html";
-    private ExtentReports extent;
-
-
-
-public void generateReport(List xmlSuites, List suites, String outputDirectory) {
-        init(OUTPUT_FOLDER);
-        for (Object suite : suites) {
-            Map<String, ISuiteResult> result = ((ISuite) suite).getResults();
-            for (ISuiteResult r : result.values()) {
-                ITestContext context = r.getTestContext();
-                if (context != null) {
-                    buildTestNodes(context.getPassedTests(), Status.PASS);
-                    buildTestNodes(context.getFailedTests(), Status.FAIL);
-                    buildTestNodes(context.getSkippedTests(), Status.SKIP);
-                }
-            }
-        }
-        for (String s : Reporter.getOutput()) {
-            extent.addTestRunnerOutput(s);
-        }
-        extent.flush();
-    }
-
-
-    private void init(String outputDirectory) {
-        ExtentSparkReporter htmlReporter = new ExtentSparkReporter(outputDirectory + "/" + FILE_NAME);
-        htmlReporter.config().setDocumentTitle("ExtentSparkReports - Created by Automation Script");
-        htmlReporter.config().setReportName("ExtentSparkReports - Created by Automation Script");
-        htmlReporter.config().setTheme(Theme.STANDARD);
+public class ExtentTestNGIReporterListener {
+    static ExtentReports extent;
+    public static ExtentReports extentReportGenerator(){
+        String path = System.getProperty("user.dir")+"\\ScreenShotReports\\ExtentReport.html";
+        ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+        reporter.config().setDocumentTitle("ExtentSparkReports - Created For Zensar");
+        reporter.config().setReportName("ExtentSparkReports - BFSI");
         extent = new ExtentReports();
-        extent.attachReporter(htmlReporter);
         extent.setSystemInfo("Application", "Eform And Hrm");
         extent.setSystemInfo("Environment", "Dev");
         extent.setSystemInfo("Execution Mode", "Automated");
-        extent.setReportUsesManualConfiguration(true);
-    }
-    public static void assignCategories(ExtentTest test, ITestResult result) {
-        for (String group : result.getMethod().getGroups()) {
-            test.assignCategory(group);
-        }
-    }
-    private void buildTestNodes(IResultMap tests, Status status) {
-        ExtentTest test = null;
-
-        if (tests.size() > 0) {
-            for (ITestResult result : tests.getAllResults()) {
-                test.getModel().setDescription("Method Name is: " + result.getMethod().getMethodName());
-                test.getModel().setStartTime(getTime(result.getStartMillis()));
-                test.getModel().setEndTime(getTime(result.getEndMillis()));
-                test.getModel().timeTaken();
-                assignCategories(test, result);
-
-                if (result.getStatus() == ITestResult.FAILURE) {
-                    test.log(status.FAIL, "TEST CASE IS FAILED on " + result.getName() + " Method"); //to add name in extent report
-                    test.log(status, result.getThrowable());
-                } else if (result.getStatus() == ITestResult.SKIP) {
-                    test.log(status.SKIP, "Test Case IS SKIPPED on " + result.getName() + "Method");
-                } else if (result.getStatus() == ITestResult.SUCCESS) {
-                    test.log(status.PASS, "Test Case IS PASSED on " + result.getName() + " Method");
-                }
-            }
-        }
-
+        extent.attachReporter(reporter);
+        return extent;
     }
 
-    private Date getTime(long millis) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(millis);
-        return calendar.getTime();
-
+    public static String getScreenShotPath(String TestCaseName) throws IOException {
+        TakesScreenshot ts = (TakesScreenshot) DriverFactory.getDriver();
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        String destpath = System.getProperty("user.dir")+"\\ScreenShotReports\\"+TestCaseName+".png";
+        File file = new File(destpath);
+        FileUtils.copyFile(source,file);
+        return destpath;
     }
 }
-*/
--- src/main/java/utils/ScreenShot.java
+++ src/main/java/utils/ScreenShot.java
@@ -4,27 +4,49 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static BrowserFactory.DriverFactory.tlDriver;
+
 public class ScreenShot {
-
-    public static String getScreenshot(WebDriver driver) {
-        TakesScreenshot ts = (TakesScreenshot) driver;
-
-        File src = ts.getScreenshotAs(OutputType.FILE);
-
-        String path = System.getProperty("user.dir") + "/Screenshot/" + System.currentTimeMillis() + ".png";
-
-        File destination = new File(path);
-
+   /* public static String takeScreenshotAtEndOfTest()  {
+        String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        TakesScreenshot ts = (TakesScreenshot)tlDriver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        String destination = System.getProperty("user.dir") + "/screenshots/" +  dateName
+                + ".png";
+        File finalDestination = new File(destination);
         try {
-            FileUtils.copyFile(src, destination);
+            FileHandler.copy(source, finalDestination);
         } catch (IOException e) {
-            System.out.println("Capture Failed " + e.getMessage());
+            e.printStackTrace();
         }
+        return destination;
+    }*/
+   public static String getScreenshot(WebDriver driver)
+
+    {
+        TakesScreenshot ts=(TakesScreenshot) driver;
+
+        File src=ts.getScreenshotAs(OutputType.FILE);
+
+        String path=System.getProperty("user.dir")+"/Screenshot/"+System.currentTimeMillis()+".png";
+
+        File destination=new File(path);
+
+        try
+        {
+            FileUtils.copyFile(src, destination);
+        } catch (IOException e)
+        {
+            System.out.println("Capture Failed "+e.getMessage());
+        }
+
         return path;
     }
 }

929a99f7823edf99cf7a9e2f5a0818c3b7ec1821
anil-kumar-automation <anil.singala@zensar.com>
2023-01-25 22:26:32 +0530

    new extent report with logs

---
+++ src/main/java/listeners/ExtentionUtil.java
@@ -0,0 +1,22 @@
+package listeners;
+
+import org.testng.ITestResult;
+
+public class ExtentionUtil {
+    public static TestStatus processStatus(ITestResult result){
+        TestStatus status = null;
+        if (result.getStatus() == ITestResult.SUCCESS)
+            status = TestStatus.PASS;
+        else if (result.getStatus() == ITestResult.FAILURE)
+            status = TestStatus.FAIL;
+        else
+            status = TestStatus.SKIP;
+        return  status;
+
+    }
+
+    public enum TestStatus {
+        FAIL,SKIP,PASS;
+    }
+}
+

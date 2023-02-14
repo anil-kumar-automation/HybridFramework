58284b70352011fcec29c1999f78ada9776aeb2c
Srimannarayana Kode <s.kode@zensar.com>
2023-02-14 16:38:36 +0530

    Parallel execution of Testclasses Dynamically

--- src/test/java/tests/ExcelTestRunner.java
+++ src/test/java/tests/ExcelTestRunner.java
@@ -1,61 +1,139 @@
 package tests;
 
-import org.testng.TestNG;
-import org.testng.xml.XmlClass;
-import org.testng.xml.XmlSuite;
-import org.testng.xml.XmlTest;
-
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
+import org.testng.annotations.Test;
+import org.testng.xml.XmlClass;
+import org.testng.xml.XmlInclude;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
+
 
 public class ExcelTestRunner {
 
-    public static void main(String[] args) throws IOException {
-        // Open the Excel file
-        FileInputStream fis = new FileInputStream("src/test/resources/TestData/DynamicTestsHrm.xlsx");
-        Workbook workbook = new XSSFWorkbook(fis);
-        Sheet sheet = workbook.getSheetAt(0);
+    public static void main(String[] args) throws ClassNotFoundException {
+        String excelPath ="C:\\Users\\SK66921\\Documents\\Git\\HybridFramework\\src\\test\\resources\\TestData\\DynamicMulti.xlsx";
+        // Read the class names from the Excel sheet
+        List<String> classNames = ExcelUtils.getClassNamesFromExcelSheet(excelPath,"Sheet1");
 
-        // Create a TestNG suite
+        // Create TestNG object
+        TestNG testng = new TestNG();
+
+        // Create a TestListenerAdapter to listen to test results
+        TestListenerAdapter tla = new TestListenerAdapter();
+
+        // Add the TestListenerAdapter to TestNG
+        testng.addListener(tla);
+
+        // Create a new test suite and classes running in parallel
         XmlSuite suite = new XmlSuite();
-        suite.setName("TestSuite");
+        suite.setName("Dynamic Test Suite");
         suite.setParallel(XmlSuite.ParallelMode.CLASSES);
 
-        // Create a TestNG test
+        // Create a new test with the specified class names
         XmlTest test = new XmlTest(suite);
-        test.setName("Test");
+        test.setName("Dynamic Test");
+        test.setXmlClasses(getXmlClasses(classNames));
 
-        // Create a list of XmlClass objects
-        List<XmlClass> classes = new ArrayList<XmlClass>();
+        // Add the test to the suite
+        suite.setTests(new ArrayList<XmlTest>() {{
+            add(test);
+        }});
 
-        // Iterate through the rows of the Excel sheet
-        for (Row row : sheet) {
-            Cell cell = row.getCell(0);
-            String className = cell.getStringCellValue();
-            System.out.println(className);
-            classes.add(new XmlClass(className));
-        }
-
-        // Set the list of XmlClass objects to the test
-        test.setXmlClasses(classes);
-
-        // Create a list of XmlSuite objects
-        List<XmlSuite> suites = new ArrayList<XmlSuite>();
+        // Create a list of suites to run
+        List<XmlSuite> suites = new ArrayList<>();
         suites.add(suite);
 
-        // Set the list of XmlSuite objects to TestNG
-        TestNG tng = new TestNG();
-        tng.setXmlSuites(suites);
+        // Set the suites to run on TestNG
+        testng.setXmlSuites(suites);
 
         // Run the tests
-        tng.run();
+        testng.run();
+
+        // Get the results
+        List<ITestResult> passedTests = tla.getPassedTests();
+        List<ITestResult> failedTests = tla.getFailedTests();
+        List<ITestResult> skippedTests = tla.getSkippedTests();
+
+        // Print the results
+        System.out.println("Passed Tests:");
+        for (ITestResult result : passedTests) {
+            System.out.println(result.getName() + " passed");
+        }
+
+        System.out.println("\nFailed Tests:");
+        for (ITestResult result : failedTests) {
+            System.out.println(result.getName() + " failed");
+        }
+
+        System.out.println("\nSkipped Tests:");
+        for (ITestResult result : skippedTests) {
+            System.out.println(result.getName() + " skipped");
+        }
+    }
+
+    /**
+     * Get a list of XmlClasses for the specified class names.
+     */
+    private static List<XmlClass> getXmlClasses(List<String> classNames) throws ClassNotFoundException {
+        List<XmlClass> xmlClasses = new ArrayList<>();
+        for (String className : classNames) {
+            XmlClass xmlClass = new XmlClass(className);
+            List<XmlInclude> xmlIncludes = new ArrayList<>();
+            Method[] methods = Class.forName(className).getMethods();
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(Test.class)) {
+                    Test annotation = method.getAnnotation(Test.class);
+                    if (annotation != null) {
+                        int priority = annotation.priority();
+                        XmlInclude xmlInclude = new XmlInclude(method.getName(), priority);
+                        xmlIncludes.add(xmlInclude);
+                    }
+                }
+            }
+            xmlClass.setIncludedMethods(xmlIncludes);
+            xmlClasses.add(xmlClass);
+        }
+        return xmlClasses;
     }
 }
+class ExcelUtils {
+    public static List<String> getClassNamesFromExcelSheet(String fileName, String sheetName) {
+        List<String> classNames = new ArrayList<>();
+        try {
+            FileInputStream inputStream = new FileInputStream(new File(fileName));
+            Workbook workbook = WorkbookFactory.create(inputStream);
+            Sheet sheet = workbook.getSheet(sheetName);
+            Iterator<Row> rowIterator = sheet.rowIterator();
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Cell cell = row.getCell(0);
+                if (cell != null) {
+                    String className = cell.getStringCellValue();
+                    classNames.add(className);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return classNames;
+    }
+}
+
+
+
+
+
--- src/test/java/tests/PimHrmTestcases.java
+++ src/test/java/tests/PimHrmTestcases.java
@@ -28,109 +28,94 @@ public class PimHrmTestcases {
         Log.info(message);
     }
 
-    @Test
-    public void launch() throws IOException, InterruptedException, AWTException {
+    @Test(priority = 1)
+    public void launch() throws IOException {
         df = new DriverFactory();
         df.init_driver("edge");
         CommonActions.navigateOrangeHrmmurl();
         info("entering Orange HRM application URL");
         //log.info("entering Orange HRM application URL");
+    }
+
+    @Test(priority = 2)
+    public void login() throws IOException, InterruptedException {
         loginForHrm = new LoginForHrmPage(DriverFactory.getDriver());
         loginForHrm.setLogindetail();
         info("entering credential on Orange HRM application");
+    }
+
+    @Test(priority = 3)
+    public void clickPimAction() throws InterruptedException {
         aep = new AddEmployeeForHrmPage(DriverFactory.getDriver());
         Thread.sleep(3000);
         aep.clickPIM();
         info("clicking PIM On Orange HRM application");
         Thread.sleep(2000);
+    }
+
+    @Test(priority = 4)
+    public void clickAddPageAction() throws InterruptedException {
         aep = new AddEmployeeForHrmPage(DriverFactory.getDriver());
         aep.clickAddEmployee();
         info("clicking AddEmployee On Orange HRM application");
         Thread.sleep(2000);
+    }
+
+
+    @Test(priority = 5)
+    public void AddPageAction() throws IOException, AWTException, InterruptedException {
         aep = new AddEmployeeForHrmPage(DriverFactory.getDriver());
         aep.AddEmployeeActions();
         info("entering Orange HRM application details as First Name,lastName, middleName and click save button");
-        aep = new AddEmployeeForHrmPage(DriverFactory.getDriver());
-        aep.AddEmployeeActions();
-        info("entering Orange HRM application details as First Name,lastName, middleName and click save button");
+    }
+
+    @Test(priority = 6)
+    public void scrolling() throws InterruptedException {
         aep = new AddEmployeeForHrmPage(DriverFactory.getDriver());
         Thread.sleep(3000);
         aep.scrool();
         Thread.sleep(3000);
         info("scrolled Orange HRM application ");
+    }
+
+    @Test(priority = 7)
+    public void DropdownAction() throws InterruptedException {
         aep = new AddEmployeeForHrmPage(DriverFactory.getDriver());
         aep.dropdown();
         info("selecting dropdowns on Orange HRM application");
+    }
+
+    @Test(priority = 8)
+    public void SelectData() throws InterruptedException {
         aep = new AddEmployeeForHrmPage(DriverFactory.getDriver());
         aep.setData();
         info("selected data on Hrm application");
+    }
+
+    @Test(priority = 9)
+    public void PersonalGenderSmoker() throws InterruptedException {
         aep = new AddEmployeeForHrmPage(DriverFactory.getDriver());
         aep.SelectRadioButton();
         info("Select Radio Button on Hrm");
         aep.SelectCheckboxAndSave();
         info("Select Smoker checkbox on Hrm");
         Thread.sleep(2000);
-        CommonActions.tearDown();
-        info("closing Orange HRM application browser");
-    }
-
-  /*  @Test(priority = 2)
-    public void login() throws IOException, InterruptedException {
-
-    }
-
-    @Test(priority = 3)
-    public void clickPimAction() throws InterruptedException {
-
-    }
-
-    @Test(priority = 4)
-    public void clickAddPageAction() throws InterruptedException {
-
-    }
-
-
-    @Test(priority = 5)
-    public void AddPageAction() throws IOException, AWTException, InterruptedException {
-
-    }
-
-    @Test(priority = 6)
-    public void scrolling() throws InterruptedException {
-
-    }
-
-    @Test(priority = 7)
-    public void DropdownAction() throws InterruptedException {
-
-    }
-
-    @Test(priority = 8)
-    public void SelectData() throws InterruptedException {
-
-    }
-
-    @Test(priority = 9)
-    public void PersonalGenderSmoker() throws InterruptedException {
-
     }
 
     @Test(priority = 10)
     public void quit() {
-        *//*if(result.getStatus()==result.FAILURE || result.getStatus()==result.SKIP) {
+        /*if(result.getStatus()==result.FAILURE || result.getStatus()==result.SKIP) {
             String screenshotPath = util.captureScreenshot(driver, result.getName());
             result.setAttribute("screenshotPath", screenshotPath); //sets the value the variable/attribute screenshotPath as the path of the sceenshot
-        }*//*
-       *//*if(result.getStatus()==ITestResult.FAILURE)
+        }*/
+       /*if(result.getStatus()==ITestResult.FAILURE)
         {
             String temp= getScreenshot(DriverFactory.getDriver());
-
             test.fail(result.getThrowable().getMessage(), MediaEntityBuilder.createScreenCaptureFromPath(temp).build());
         }
         extent.flush();
-*//*
-
-        */
+*/
+        CommonActions.tearDown();
+        info("closing Orange HRM application browser");
     }
-
-
+}
